40db1a9b30f72e1e162da92b70c8df92753e3dae
Evgenii <zubarev.e.v@gmail.com>
2014-07-15 15:59:08 +0400

    some updates in c + output files

--- main.cpp
+++ main.cpp
@@ -8,18 +8,20 @@ using namespace std;
 #include <stdlib.h>
 
 const double PI = M_PI;
-#define N 14 //number steps
-#define ARG_N      65536 //how much 360 degrees in int
+#define     N       14 //number steps
+#define AMP_MSB     14
+#define ARG_MSB     16
+//#define ARG_N       65536 //how much 360 degrees in int
 
-void calc_cordic(int& re, int& im, int module, double angle) {
+void calc_cordic(int& re, int& im, int module, int angle) {
     double d_angle[N];
+    int i_angle[N];
     double sum_angles = .0f;
-    //int tst_an = 0;
     for(int k = 0; k < N; k++) {
         d_angle[k] = atan(1.0/pow(2,k)) * (180/PI);
         sum_angles += d_angle[k];
         //printf("%f\n", d_angle[k]);
-        //tst_an = d_angle[k]*ARG_N/360.0f;
+        i_angle[k] = d_angle[k]*(1 << ARG_MSB)/360.0f;
         //printf("%d\n", tst_an);
     }
 
@@ -36,25 +38,20 @@ void calc_cordic(int& re, int& im, int module, double angle) {
     int Re[N];
     int Im[N];
     int CORDIC_GAIN = cordic_gain*module;
-
     //printf("%d\n", CORDIC_GAIN);
 
     Re[0] = CORDIC_GAIN;
     Im[0] = CORDIC_GAIN;
 
-    double my_arg = angle;
-    if(my_arg >= 90.0f && my_arg < 180) {
-        my_arg -= 90.0f;
-    }
-    if(my_arg >= 180.0f && my_arg < 270) {
-        my_arg -= 180.0f;
-    }
-    if(my_arg >= 270.0f && my_arg < 360) {
-        my_arg -= 270.0f;
-    }
 
-    double ARG[N];
-    ARG[0] = 45.0f;
+    //int quad = (angle & 0xC000 ) >> 14;
+    //angle = angle & 0x3FFF;
+
+    int quad = (angle & (3 << (ARG_MSB-2))) >> (ARG_MSB-2);
+    angle = angle & ((1 << (ARG_MSB-2)) -1);
+
+    int ARG[N];
+    ARG[0] = i_angle[0];
 
 
     //printf("%f\t\t%d\t%d\t\n", ARG[0], Re[0], Im[0]);
@@ -63,21 +60,20 @@ void calc_cordic(int& re, int& im, int module, double angle) {
     int tmp_Im = 0;
     int tst = 0;
     for(int i = 1; i < N; i++) {
-        tmp_i = int(pow(2,i-1));
-        if(ARG[i-1] > my_arg) {
-            tmp_Im = Im[i-1] + tmp_i;
-            tmp_Re = Re[i-1] + tmp_i;
-            //Re[i] = Re[i-1] + (tmp_Im >> i);
-            Re[i] = Re[i-1] + (tmp_Im >> i);
-            Im[i] = Im[i-1] - (tmp_Re >> i);
-            ARG[i] = ARG[i-1] - d_angle[i];
+        tmp_i = 1 << (i-1);
+        if(ARG[i-1] > angle) {
+            tmp_Im  =   Im[i-1] + tmp_i;
+            tmp_Re  =   Re[i-1] + tmp_i;
+            Re[i]   =   Re[i-1] + (tmp_Im >> i);
+            Im[i]   =   Im[i-1] - (tmp_Re >> i);
+            ARG[i]  =   ARG[i-1] - i_angle[i];
         }
         else {
-            tmp_Im = Im[i-1] + tmp_i;
-            tmp_Re = Re[i-1] + tmp_i;
-            Re[i] = Re[i-1] - (tmp_Im >> i);
-            Im[i] = Im[i-1] + (tmp_Re >> i);
-            ARG[i] = ARG[i-1] + d_angle[i];
+            tmp_Im  =   Im[i-1] + tmp_i;
+            tmp_Re  =   Re[i-1] + tmp_i;
+            Re[i]   =   Re[i-1] - (tmp_Im >> i);
+            Im[i]   =   Im[i-1] + (tmp_Re >> i);
+            ARG[i]  =   ARG[i-1] + i_angle[i];
         }
     }
 
@@ -90,23 +86,20 @@ void calc_cordic(int& re, int& im, int module, double angle) {
     int tmp;
 
 
-    if(angle >= 90.0f && angle < 180) {
+    if(quad == 1) {
         tmp = Re[N-1];
         Re[N-1] = -Im[N-1];
         Im[N-1] = tmp;
-        ARG[N-1] += 90.0;
     }
-    if(angle >= 180.0f && angle < 270) {
+    if(quad == 2) {
         tmp = Re[N-1];
         Re[N-1] = -Re[N-1];
         Im[N-1] = -Im[N-1];
-        ARG[N-1] += 180.0;
     }
-    if(angle >= 270.0f && angle < 360) {
+    if(quad == 3) {
         tmp = Re[N-1];
         Re[N-1] = Im[N-1];
         Im[N-1] = -tmp;
-        ARG[N-1] += 270.0;
     }
 
     int tmp_cos = Re[N-1] - icos;
@@ -119,7 +112,7 @@ void calc_cordic(int& re, int& im, int module, double angle) {
 
     //static int max_delta_cos = tmp_cos > max_delta_cos ? tmp_cos : max_delta_cos;
     //static int max_delta_sin = tmp_sin > max_delta_sin ? tmp_sin : max_delta_sin;
-    //printf("%f\t%d\t%d", ARG[N-1], re, im);
+    //printf("%d\t%d\t%d", ARG[N-1], re, im);
 }
 
 int main() {
@@ -135,12 +128,13 @@ int main() {
     int max_arg = 0;
     int disp_cos = 0;
     int disp_sin = 0;
-    for(int i = 0; i < ARG_N; i++) {
-    //for(int i = 0; i < 360000; i++) {
-        double  my_angle = (double)i*360.0/ARG_N;
-        //double  my_angle = (double)i/1000.0;
-        int     my_modul = 8191;
-        calc_cordic(Re,Im, my_modul, my_angle);
+
+ //   calc_cordic(Re, Im, 8191, 1001);
+
+    for(int i = 0; i < (1<<ARG_MSB); i++) {
+        double  my_angle = (double)i*360.0/(1<<ARG_MSB);
+        int     my_modul = 1 << (AMP_MSB-1);
+        calc_cordic(Re,Im, my_modul, i);
 
         double mcos = cos(my_angle*PI/180);
         double msin = sin(my_angle*PI/180);
@@ -153,16 +147,11 @@ int main() {
         tmp_cos = Re - icos;
         tmp_sin = Im - isin;
 
-        cos_delta_file << tmp_cos << endl;
-        sin_delta_file << tmp_sin << endl;
+        cos_delta_file << Re << endl;
+        sin_delta_file << Im << endl;
 
         //printf("\t%d\t%d\t%d\t%d\n",icos, isin, tmp_cos, tmp_sin);
         console << tmp_cos <<  endl;//"\t" << tmp_sin<< endl;// << "\t" << tmp_sin << "\t" << endl;
-        //tmp_cos = tmp_cos < 0 ? -tmp_cos : tmp_cos;
-        //tmp_sin = tmp_sin < 0 ? -tmp_sin : tmp_sin;
-
-
-        max_arg = tmp_cos > max_delta_re ? i : max_arg;
         max_delta_re = abs(tmp_cos) > abs(max_delta_re) ? tmp_cos : max_delta_re;
         max_delta_im = abs(tmp_sin) > abs(max_delta_im) ? tmp_sin : max_delta_im;
 
